d9fb310b9b4c505f5246aeb9180f7d4c906e074a
IncapableFury <41422991+IncapableFury@users.noreply.github.com>
2018-10-12 21:50:18 -0400

    Rename ultimateUeslessDictionary.java to UltimateUeslessDictionary.java

---
+++ UltimateUeslessDictionary.java
@@ -0,0 +1,32 @@
+package hw3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ultimateUeslessDictionary {
+
+    public static File file = new File("dict.txt");
+    public static Scanner s = new Scanner(System.in);
+    public static String word = "";
+    public static HashMap dict = new HashMap();
+
+    public static void main(String[] args) throws IOException {
+        readFileIntoDict(file,dict);
+        System.out.println("What are you looking for?");
+        word = s.next();
+//
+        if (dict.containsKey(word)) {
+            System.out.println("Okay we have it, but we don't what does it mean either");
+        } else {
+            System.out.println("Mis-spelled,why don't you google it?");
+        }
+    }
+
+    public static void readFileIntoDict(File file,HashMap dict) throws FileNotFoundException {
+//here will be completed by my teammate
+    }
+}
